1b26406fc7d3d09521295df9caaf962ff0a170ab
MrArwut <mrarwut.null@gmail.com>
2018-04-09 14:33:24 +0700

    xCode 9/4/61 14.30

--- PolyTech/ViewController.swift
+++ PolyTech/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblNickName: UILabel!
+    @IBOutlet weak var txtNickName: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btClick(_ sender: UIButton) {
+        lblNickName.text = txtNickName.text
+    }
+    
+    
 
 }
 
